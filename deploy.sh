164eb24d47b1efe1e892c13be54952e29378f16e
Daniele122898 <d.palbanese@gmail.com>
2019-04-12 23:19:19 +0200

    lets try again

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 set -e
 
 ssh_run() {
-    ssh -oStrictHostKeyChecking=no -i ./travis.key ${SSH_USER}@${SSH_HOST} "$1"
+    ssh -oStrictHostKeyChecking=no -i ./tra.key ${SSH_USER}@${SSH_HOST} "$1"
 }
 
-openssl aes-256-cbc -K $encrypted_16292a871be3_key -iv $encrypted_16292a871be3_iv -in travis.key.enc -out travis.key -d
-chmod 0600 ./travis.key
+openssl aes-256-cbc -K $encrypted_16292a871be3_key -iv $encrypted_16292a871be3_iv -in tra.key.enc -out tra.key -d
+chmod 0600 ./tra.key
 
 ssh_run "systemctl --user stop sora-request.service"
 
-rsync -avzh -e 'ssh -oStrictHostKeyChecking=no -i ./travis.key' --exclude node_modules --exclude .git --exclude design . ${SSH_USER}@${SSH_HOST}:${DEPLOY_PATH}
+rsync -avzh -e 'ssh -oStrictHostKeyChecking=no -i ./tra.key' --exclude node_modules --exclude .git . ${SSH_USER}@${SSH_HOST}:${DEPLOY_PATH}
 ssh_run "sh -c 'cd ${DEPLOY_PATH} && yarn install'"
 
 ssh_run "systemctl --user start sora-request.service"
