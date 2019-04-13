12749ae1ee4d6dd4ae1aab229a7e8657bb812b91
Daniele122898 <d.palbanese@gmail.com>
2019-04-13 23:17:09 +0200

    this fkin hamburger is borked

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { IoMdMenu, IoMdClose } from 'react-icons/io';
+import { slide as Menu } from 'react-burger-menu';
 import { startLogout } from "../actions/auth";
 import { AnyThunkDispatch } from '../types'
 import { ApplicationState } from '../store/index';
@@ -8,15 +10,9 @@ import { isAdmin } from '../utils/utils';
 
 type RootState = {};
 
-export const Header = ({ startLogout, isAdmin }: any) => (
-  <header className="header">
-    <div className="content-container">
-      <div className="header__content">
-        <Link className="header__title" to="/dashboard">
-          <h1>Sora Requests</h1>
-        </Link>
-        <div>
-          { isAdmin &&
+/*
+
+{ isAdmin &&
             <NavLink
               to='/adminPanel'
               className="button button--link header--button" 
@@ -36,6 +32,47 @@ export const Header = ({ startLogout, isAdmin }: any) => (
           >
             Logout
           </button>
+
+*/
+
+export const Header = ({ startLogout, isAdmin }: any) => (
+  <header className="header">
+    <div className="content-container">
+      <div className="header__content">
+        <Link className="header__title" to="/dashboard">
+          <h1>Sora Requests</h1>
+        </Link>
+        <div id={"outer-container"}>
+          
+          
+
+          <Menu
+            disableAutoFocus={'true'}
+            pageWrapId={'app'}
+            outerContainerId={'outer-container'}
+            right 
+            width={'50%'}
+            customBurgerIcon={<IoMdMenu/>}
+            customCrossIcon={<IoMdClose/>}
+            crossButtonClassName={'cross'}
+            burgerButtonClassName={'burger'}
+            bodyClassName={'BODY'}
+            htmlClassName={'HTML'}
+            overlayClassName={'OVERLAY'}
+            menuClassName={'burger-menu'}
+          >
+            <a href="" 
+              className="button button--link burger-button"
+            >
+              My Requests
+            </a>
+            <a href="" 
+              className="button button--link burger-button"
+            >
+              Logout
+            </a>
+          </Menu>
+
         </div>
       </div>
     </div>
