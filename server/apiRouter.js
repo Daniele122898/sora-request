b8e683e17ffc77b836436801bf83b426fde0b18f
Serenity <d.palbanese@gmail.com>
2020-05-28 22:14:08 +0200

    Refactor tabbing, Added seperate endpoints to accept or reject a waifu, added helper method, do owner check

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -2,10 +2,11 @@ const router = require('express').Router();
 const axios = require('axios');
 
 const soraPort = process.env.NODE_ENV === 'production' ? 8000 : 8100;
+const ownerId = '192750776005689344';
 
 const authCheck = (req, res, next) => {
-    if(req.user) {
-      next();
+    if (req.user) {
+        next();
     } else {
         if (req.originalUrl.includes('api')) {
             res.status(401).send('Unauthorized');
@@ -15,72 +16,102 @@ const authCheck = (req, res, next) => {
     }
 };
 
+
+const handleError = (res, error) => {
+    if (error.response) {
+        // The request was made and the server responded with a
+        // status code that falls out of the range of 2xx
+        res.status(error.response.status).send(error.response.data)
+    } else {
+        res.status(500).send('Something broke ;_;')
+    }
+};
+
 router.get('/checkLogin', (req, res) => {
     if (req.user) {
-      res.json({
-        login: true,
-        user: req.user
-      });
+        res.json({
+            login: true,
+            user: req.user
+        });
     } else {
-      res.json({
-        login: false
-      });
+        res.json({
+            login: false
+        });
     }
 });
 
-router.post('/requestApproval', authCheck, (req, res) => {
-  let request = {
-    ...req.body,
-    userId: req.user.id
-  };
+router.post('/request/:requestId/approve', authCheck, (req, res) => {
+    const requestId = req.params.requestId;
+    const userId = req.user.id
+    if (userId != ownerId) {
+        res.status(401).send("Only Sora admin can accept or reject requests!")
+    }
 
-  axios.post(`http://localhost:${soraPort}/api/Waifu/requestApproval`, request)
-    .then(r => {
-        res.json(r.data);
-    })
-    .catch(e=> {
-        console.log(e);
-        res.status(500).send('Something broke ;_;')
-    });
+    axios.post(`http://localhost:${soraPort}/api/requests/${requestId}/approve`, request)
+        .then(r => {
+            res.status(200).send();
+        })
+        .catch(e => {
+            console.log(e);
+            handleError(res, e);
+        });
+});
+
+
+router.post('/request/:requestId/reject', authCheck, (req, res) => {
+    const requestId = req.params.requestId;
+    const userId = req.user.id
+    if (userId != ownerId) {
+        res.status(401).send("Only Sora admin can accept or reject requests!")
+    }
+
+    axios.post(`http://localhost:${soraPort}/api/requests/${requestId}/reject`, request)
+        .then(r => {
+            res.status(200).send();
+        })
+        .catch(e => {
+            console.log(e);
+            handleError(res, e);
+        });
 });
 
 router.get('/getAllRequests', authCheck, (req, res) => {
-  axios.get(`http://localhost:${soraPort}/api/requests/user/${req.user.id}`)
-    .then(r => {
-        res.json(r.data);
-    })
-    .catch(e=> {
-        console.log(e);
-        res.status(500).send('Something broke ;_;')
-    });
+    axios.get(`http://localhost:${soraPort}/api/requests/user/${req.user.id}`)
+        .then(r => {
+            res.json(r.data);
+        })
+        .catch(e => {
+            console.log(e);
+            handleError(res, e);
+        });
 });
 
 router.get('/getAdminRequests', authCheck, (req, res) => {
 
-  axios.get(`http://localhost:${soraPort}/api/requests`)
-    .then(r => {
-        res.json(r.data);
-    })
-    .catch(e=> {
-        console.log(e);
-        res.status(500).send('Something broke ;_;')
-    });
+    axios.get(`http://localhost:${soraPort}/api/requests`)
+        .then(r => {
+            res.json(r.data);
+        })
+        .catch(e => {
+            console.log(e);
+            handleError(res, e);
+        });
 });
 
 router.post('/editWaifu', authCheck, (req, res) => {
-  let request = {
-    ...req.body,
-    userId: req.user.id
-  };
+    let request = {
+        ...req.body,
+        userId: req.user.id
+    };
 
-  axios.post(`http://localhost:${soraPort}/api/requests/${requestId}`, request)
-    .then(r => {
-        res.json(r.data);
-    })
-    .catch(e=> {
-        console.log(e);
-        res.status(500).send('Something broke ;_;')
-    });
+    axios.post(`http://localhost:${soraPort}/api/requests/${requestId}`, request)
+        .then(r => {
+            res.json(r.data);
+        })
+        .catch(e => {
+            console.log(e);
+            handleError(res, e);
+        });
 });
 
 router.post('/requestWaifu', authCheck, (req, res) => {
@@ -90,29 +121,29 @@ router.post('/requestWaifu', authCheck, (req, res) => {
     };
 
     axios.post(`http://localhost:${soraPort}/api​/requests​/user​/${req.user.id}`, request)
-    .then(r => {
-        res.json(r.data);
-    })
-    .catch(e=> {
-        console.log(e);
-        res.status(500).send('Something broke ;_;')
-    });
+        .then(r => {
+            res.json(r.data);
+        })
+        .catch(e => {
+            console.log(e);
+            handleError(res, e);
+        });
 });
 
 router.post('/setNotify', authCheck, (req, res) => {
-  let request = {
-    ...req.body,
-    userId: req.user.id
-  };
+    let request = {
+        ...req.body,
+        userId: req.user.id
+    };
 
-  axios.post(`http://localhost:${soraPort}/api/requests/user/${req.user.id}/notify`, request)
-  .then(r=> {
-    res.json(r.data);
-  })
-  .catch(e=> {
-    console.log(e);
-    res.status(500).send('Something broke');
-  });
+    axios.post(`http://localhost:${soraPort}/api/requests/user/${req.user.id}/notify`, request)
+        .then(r => {
+            res.json(r.data);
+        })
+        .catch(e => {
+            console.log(e);
+            handleError(res, e);
+        });
 
 });
 
